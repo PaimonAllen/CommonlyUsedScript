5cad479b96e937015d2d318ef7dd29d7c630e8e8
ZhuoranT <10852336@qq.com>
2022-10-16 15:24:42 +0800

    update colab

--- rename.py
+++ rename.py
@@ -7,19 +7,21 @@ import os
 
 PATH = os.path.abspath(os.path.join(os.getcwd(), "dataset_copy"))
 
+
 def rename_file(oldname, val):
     oldname = os.path.join(PATH, oldname)
-    # newname = oldname 
+    # newname = oldname
     print(oldname)
     newname = f'test{val}.jpg'
     # newname = os.path.join(PATH,newname)+'.txt'
-    newname = os.path.join(PATH,newname)
+    newname = os.path.join(PATH, newname)
     # newname = newname+"1"
 
     os.rename(oldname, newname)
     # print("oldname -------------> newname ", oldname,newname)
- 
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     fileList = os.listdir(PATH)
     fileList.sort()
     print(fileList)
@@ -28,5 +30,3 @@ if __name__=="__main__":
         print(i)
         rename_file(name, i)
         i += 1
-
-
